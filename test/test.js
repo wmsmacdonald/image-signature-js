86cd8265940fb4e204867a97f2a2caab1237ac91
William MacDonald <wmsmacdonald@gmail.com>
2017-01-02 00:28:33 -0600

    added test for grayscale

--- test/test.js
+++ test/test.js
@@ -5,9 +5,23 @@ const assert = require('assert');
 const imageSignature = require('../');
 
 describe('imageSignature', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+  describe('#grayscale()', function() {
+    it('should return an array a quarter of the size with the average of RGB values', function() {
+      let size = 100;
+      let value = 40;
+      let imageData = new Uint8ClampedArray(size).fill(value);
+      let grayscaled = imageSignature.greyscale(imageData);
+      assert.strictEqual(grayscaled.length, size / 4, 'result array must be a quarter of the size');
+      let allSame = grayscaled.every(el => el === value);
+      assert(allSame, 'result array must be same as starting values");
+    });
+    it('should return an array with the proper average', function() {
+      let imageData = new Uint8ArrayClampedArray([1, 2, 3, 155, 1, 2, 2]);
+      let grayscaled = imageSignature.greyscale(imageData);
+      assert.strictEqual(grayscaled[0], 2);
+      assert.strictEqual(grayscaled[1], 5 / 3);
     });
   });
 });
+
+
