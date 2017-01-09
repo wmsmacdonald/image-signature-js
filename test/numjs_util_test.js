aec86cfdde540b0705b8a410cc536ec38f204867
William MacDonald <wmsmacdonald@gmail.com>
2017-01-08 23:07:13 -0600

    removed nj array equals

--- test/numjs_util_test.js
+++ test/numjs_util_test.js
@@ -7,31 +7,30 @@ const _ = require('underscore')
 const njUtil = require('../lib/nj_util')
 
 describe('njUtil', function() {
-  describe('array_equals', function() {
-    it('should return true for equal arrays', function() {
-      const arr1 = nj.array([1, 2, 3])
-      const arr2 = nj.array([1, 2, 3])
-      assert(njUtil.array_equals(arr1, arr2))
-    })
-    it('should return true for equal arrays', function() {
-      const arr1 = nj.array([1, 2, 3])
-      const arr2 = nj.array([1, 2, 3])
-      assert(njUtil.array_equals(arr1, arr2))
-    })
-  })
   describe('diff', function() {
     it('should return the correct array for a 3x1 array', function() {
       const arr = nj.array([1, 2, 4, 7, 0])  
       const diff = njUtil.diff(arr)  
       const expected = nj.array([1, 2, 3, -7])
-      console.log(diff)
-      assert(njUtil.array_equals(diff, expected))
+      assert(nj.equal(diff, expected))
     })
     it('should return the correct array for a 2x3 array', function() {
       const arr = nj.array([[1, 2, 3], [1, 2, 3]])  
       const diff = njUtil.diff(arr)  
       const expected = nj.array([[1, 1], [1, 1]])
-      assert(njUtil.array_equals(diff, expected))
+      assert(nj.equal(diff, expected))
+    })
+    it('should return the correct array for a 2x3 array on axis 0', function() {
+      const arr = nj.array([[1, 2, 3], [1, 2, 3]])  
+      const diff = njUtil.diff(arr, 1, 0)  
+      const expected = nj.array([[0, 0, 0]])
+      assert(nj.equal(diff, expected))
+    })
+    it('should return the correct array for a array with n = 2', function() {
+      const arr = nj.array([1, 2, 4, 7, 0])  
+      const diff = njUtil.diff(arr, 2)  
+      const expected = nj.array([1, 1, -10])
+      assert(nj.equal(diff, expected))
     })
   })
 })
