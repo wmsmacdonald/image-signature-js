4cf3cc9d770da7ed643fdc34a77bce8a11ba35e8
William MacDonald <wmsmacdonald@gmail.com>
2017-01-14 00:44:43 -0600

    working for image

--- test/test.js
+++ test/test.js
@@ -6,25 +6,29 @@ const fs = require('fs')
 
 const assert = require('assert')
 const _ = require('underscore')
-const nj = require('../bower_components/numjs/dist/numjs')
+const njPure = require('../bower_components/numjs/dist/numjs')
+const nj = require('numjs')
 const image = require('get-image-data')
 
 const imageSignature = require('../lib/image_signature')
 
 describe('imageSignature', function () {
   describe('#generate()', function () {
-    it('return array', function (cb) {
-      const buf = fs.readFileSync(__dirname + '/fixtures/Reina_restaurant_Istanbul.jpg')
-      image(buf, (err, imageData) => {
-        if (err) return cb(err)
-        //const signature = imageSignature.generate(imageData)
-      })
+    it('return array', function () {
+      const img = nj.images.read(__dirname + '/fixtures/spaceship.png')
+      const imageData = {
+        height: img.shape[0],
+        width: img.shape[1],
+        data: img.flatten().tolist()
+      }
+      const signature = imageSignature.generate(imageData)
+      signature.forEach(comparisonGroup => assert(comparisonGroup.length <= 8))
     })
   })
   describe('#autoCrop()', function () {
     it('should not return a list with the element', function () {
-      const data = nj.multiply(nj.random([100, 150]), 255)
-      const image = nj.array(data.flatten().tolist().map(el => Math.floor(el))).reshape([100, 150])
+      const data = njPure.multiply(njPure.random([100, 150]), 255)
+      const image = njPure.array(data.flatten().tolist().map(el => Math.floor(el))).reshape([100, 150])
       const cropped = imageSignature.autoCrop(image, 10, 90) 
       // should be about 90 in height
       assert(Math.abs(cropped.shape[0] - 80) < 7)
@@ -55,19 +59,19 @@ describe('imageSignature', function () {
     })
   })
   describe('#computeGridAverages()', function () {
-    it('should return the correct square for the top right corner ', function () {
+    it('should return the correct average for the top right corner ', function () {
       const image = nj.arange(1, 101).reshape(10,10)
       const neighborGroups = imageSignature.computeGridAverages(image, 10, 10)
       // square should be [1, 2, 11, 12]
-      const expected = (1 + 2 + 11 + 12) / 4
+      const expected = (12 + 13 + 22 + 23) / 4
       const result = neighborGroups.get(0,0)
       assert.strictEqual(result, expected)
     })
     it('should return the correct square for the middle', function () {
       const image = nj.arange(1, 101).reshape(10,10)
       const neighborGroups = imageSignature.computeGridAverages(image, 10, 10)
-      // square should be [45, 46, 55, 56]
-      const expected = (45 + 46 + 55 + 56) / 4
+      // square should be [56, 57, 66, 67]
+      const expected = (56 + 57 + 66 + 67) / 4
       const result = neighborGroups.get(4, 4)
       assert.strictEqual(result, expected)
     })
@@ -75,7 +79,7 @@ describe('imageSignature', function () {
       const image = nj.arange(1, 101).reshape(10,10)
       const neighborGroups = imageSignature.computeGridAverages(image, 10, 10)
       // square should be [89, 90, 99, 100]
-      const expected = (89 + 90 + 99 + 100) / 4
+      const expected = 100
       const result = neighborGroups.get(8, 8)
       assert.strictEqual(result, expected)
     })
